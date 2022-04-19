fa7600f3b8e1ecbaac9b7c96b66818df2e13bcba
jordanrjdev <62086742+jordanrjdev@users.noreply.github.com>
2022-04-18 21:47:26 -0500

    feat : init the proyect

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+app.listen(8000, () => {
+  console.log('The application is listening on port 8000!');
+});
