73302c39fff4df1c8b49f7e4841e9979dace4247
jordanrjdev <62086742+jordanrjdev@users.noreply.github.com>
2022-04-20 23:34:02 -0500

    feat : second part, query params and nodemon

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
+  if (req.query.name) {
+    // res.send(`Hello ${req.query.name}`); send response type text
+    res.json({ message: `Hello ${req.query.name}` }); //send response type json
+  } else {
+    // res.send('Hello guest'); send response type text
+    res.json({ message: 'Hello guest' }); //send response type json 
+  }
 });
 
 app.listen(8000, () => {
-  console.log('The application is listening on port 8000!');
+  console.log('Server on port 8000!');
 });
